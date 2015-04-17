496ed3b0d5a815ce07f5ed9b888d099597af4ee4
Christian J. Balcom <robot.inventor@gmail.com>
2015-04-17 12:27:40 -0400

    Started work on concept art, added some images to work from.

--- header.php
+++ header.php
@@ -33,4 +33,10 @@
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
+	<?php if ( get_header_image() ) : ?>
+	<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+		<img src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="">
+	</a>
+	<?php endif; // End header image check. ?>
+
 	<div id="content" class="site-content">
